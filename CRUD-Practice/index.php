b94c5ecda2312b3741439a3606e62433dbee5bbf
me-adon <95122572+me-adon@users.noreply.github.com>
2023-06-04 16:51:09 +0600

    Add files via upload

---
+++ CRUD-Practice/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PROJECT</title>
+</head>
+<body>
+    
+    <div>
+        <div>
+            <h2>Student Admission Form</h2>
+        </div>
+        <div>
+            <form action="" method="post">
+            <input type="text" name="std_name" placeholder="Enter Your Name"><br>
+            <input type="number" name="std_roll" placeholder="Enter Your Roll"><br>
+            <label>Browse an Image:</label><br>
+            <input type="file" name="std_image">
+            </form>
+        </div>
+       
+    </div>
+   
+    
+</body>
+</html>
